46f03186241f8be42192e938c558762a213d6e9c
niamtokik <mk@steepath.eu>
2018-02-10 11:48:16 +0100

    qad code with simple renderer...

--- friendly-router.js
+++ friendly-router.js
@@ -1,3 +1,22 @@
 new Vue({
-  el: '#app'
+    el: '#app',
+    data: { products: [] },
+    created: function() {
+	var self = this;
+	$.ajax({ url: './products.json',
+		 method: "GET",
+		 dataType: "json",
+		 success: function(data) {
+		     $.each(data, function(index, value) {
+			 $.ajax({ url: value,
+				 method: "GET",
+				 dataType: "json",
+				 success: function(data) {
+				     self.products.push(data);
+				 }})
+		     });
+		 }
+	       });
+    }
 })
+
